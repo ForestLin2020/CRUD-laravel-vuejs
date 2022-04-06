b749254b62d2b6a91eba317b4606847c6476a1e6
myusername <myusername>
2022-04-06 03:16:57 -0400

    'httpToHttps'

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,10 +3,18 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
 
         <title>CRUD-Laravel-VueJS</title>
+
+        <!-- For Localhost -->
+        <!-- <link rel="stylesheet" href="{{ asset('css/app.css') }}"> -->
+        <!-- <script src="{{ asset('js/app.js') }}" type="module" defer></script> -->
+        
+        <!-- For deployment on heroku -->
         <link rel="stylesheet" href="{{ secure_asset('css/app.css') }}">
         <script src="{{ secure_asset('js/app.js') }}" type="module" defer></script>
+
     </head>
     <body>
         <div id="app"></div>
